272923ec624a7e9f6881806316e34717e20832fd
Prateek9340 <prateeksrathore111@gmail.com>
2025-12-19 06:43:56 +0530

    Authentication updated

--- src/main/java/com/obs/config/WebConfig.java
+++ src/main/java/com/obs/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
+                .exposedHeaders("Token-Refresh")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
